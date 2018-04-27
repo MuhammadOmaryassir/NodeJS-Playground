f1c45b170c09b9f69dab871a30c469d1d871fd67
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-04-27 02:39:37 +0200

    complete register and login

---
+++ Auth-System/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://omar:12345@ds159459.mlab.com:59459/auth')
+const db = mongoose.connection
+
+// handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  // we're connected!
+  console.log("we're connected!")
+})
+
+// parse incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// include routes
+const routes = require('./routes/router')
+app.use('/api', routes)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  let err = new Error('File Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.send(err.message)
+})
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000')
+})
